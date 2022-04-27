4f4e6a69a3f6cf4b459e2c375a037f6e960a4464
taichi-de <61309248+taichi-de@users.noreply.github.com>
2022-04-27 07:49:34 +0200

    added eslint & prettier

--- src/world.ts
+++ src/world.ts
@@ -1,13 +1,21 @@
 export default class World {
-    message: string;
+  message: string
 
-    constructor(message: string){
-        this.message = message;
-    }
+  constructor(message: string) {
+    this.message = message
+  }
 
-    public sayHello(elem: HTMLElement | null){
-        if(elem){
-            elem.innerText = this.message
-        }
+  public sayHello(elem: HTMLElement | null) {
+    if (elem) {
+      elem.innerText = this.message
     }
+  }
+
+
+
+
+
+
+
+
 }
